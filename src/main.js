95873d588e76e72a5535f8cbf7f1841eaabcfe9a
Dan <danilobomfim.si@gmail.com>
2022-05-25 20:01:31 -0300

    Start

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import { createServer } from "http";
+
+const server = createServer((request, response) => {
+  switch (request.url) {
+    case "/status": {
+      response.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      response.write(
+        JSON.stringify({
+          status: "Okay",
+        })
+      );
+      response.end();
+      break;
+    }
+    default: {
+      response.writeHead(404, "Service not found.");
+      response.end();
+    }
+  }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME =  process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
+});
